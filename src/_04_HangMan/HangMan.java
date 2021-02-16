3e2dc0fa0ba20bc3fb25361f01c96896938a60ba
ezgi-b <52934686+ezgi-b@users.noreply.github.com>
2021-02-15 19:53:51 -0800

    I almost finished HangMan, maybe some debugging left.

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -26,14 +26,27 @@ public class HangMan{
 		joe.setup();
 		int numWords = Integer.parseInt(JOptionPane.showInputDialog("How many words do you want to play Hangman with?"));
 		joe.numToWordsInStack(numWords);
-		joe.wordToPanel();
-		while(!joe.ended) {
-			String g = JOptionPane.showInputDialog("Guess a letter");
-			joe.guessWord(g.charAt(0));
+		for(int i = 0; i < numWords; i++) {
+			joe.letters = "";
+			joe.gLS = "Already guessed letters: ";
+			joe.guessed.setText(joe.gLS);
+			joe.frame.pack();
+			joe.ended = false;
+			joe.guessedLetters.clear();
+			joe.wordToPanel();
+			joe.z = 0;
+			joe.man.setText(joe.hangman[joe.z]);
+			while(!joe.ended) {
+				String g = JOptionPane.showInputDialog("Guess a letter");
+				joe.guessWord(g.charAt(0));
+			}
 		}
+		JOptionPane.showMessageDialog(null, "Game over!");
+		joe.frame.dispose();
 	}
 	
 	void setup() {
+		guessed.setText(gLS);
 		hangman[0] = "<html><pre class=\\\"tab\\\">______<br>|    |<br>|   <br>     <br>    </pre></html>";
 		hangman[1] = "<html><pre class=\\\"tab\\\">______<br>|    |<br>|    0</pre></html>";
 		hangman[2] = "<html><pre class=\\\"tab\\\">______<br>|    |<br>|    0<br>     |</pre></html>";
@@ -74,33 +87,45 @@ public class HangMan{
 	}
 	
 	void guessWord(char c) {
-		if(guessing.contains(c+"")){
-			guessedLetters.push(c);
-			letters = "";
-			for(int i = 0; i<guessing.length(); i++) {
-				if(guessedLetters.contains(guessing.charAt(i))) {
-					letters+=guessing.charAt(i);
-				}else {
-					letters+="_";
-				}
+		if(Character.isLetter(c)) {
+			if(guessedLetters.contains(c)) {
+				JOptionPane.showMessageDialog(null, "You've already guessed that letter!");
+				guessed.setText(gLS);
+				frame.pack();
+			}else {
+				if(guessing.contains(c+"")){
+					guessedLetters.push(c);
+					letters = "";
+					for(int i = 0; i<guessing.length(); i++) {
+						if(guessedLetters.contains(guessing.charAt(i))) {
+							letters+=guessing.charAt(i);
+						}else {
+							letters+="_";
+						}
+						if(letters.equals(guessing)) {
+							guess.setText(letters + "  Number of letters: " + guessing.length());
+							frame.pack();
+							ended = true;
+							JOptionPane.showMessageDialog(null, "You guessed it! The word was " + guessing + "!");
+						}
 				
+					}
+					guess.setText(letters + "  Number of letters: " + guessing.length());
+				}else {
+					guessedLetters.push(c);
+					z++;
+					man.setText(hangman[z]);
+					if(z==6) {
+						ended = true;
+						JOptionPane.showMessageDialog(null, "You lost :( The word was " + guessing + "!");
+					}
+				}
+				gLS += c + ", ";
+				guessed.setText(gLS);
+				frame.pack();
 			}
-			guess.setText(letters + "  Number of letters: " + guessing.length());
-			frame.pack();
 		}else {
-			z++;
-			man.setText(hangman[z]);
-			frame.pack();
-			if(z==6) {
-				ended = true;
-				JOptionPane.showMessageDialog(null, "You lost :(");
-			}
+			JOptionPane.showMessageDialog(null, "That is not a letter! Guess again.");
 		}
-		
-		gLS += c + ", ";
-		guessed.setText(gLS);
-		frame.pack();
 	}
-	
-	
 }
